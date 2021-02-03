a034b233ee70afdc1903ba08e1988e42206786cd
Tomiwa Obanla <obanlatomiwa@gmail.com>
2021-02-03 09:48:56 +0100

    changed text in App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hey, Tommy Wayne</p>
+        <p>Hey, Mr Tommy Wayne</p>
         <a
           className="App-link"
           href="https://reactjs.org"
